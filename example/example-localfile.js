9d2aa01851cb92f679ffadadc55bebb98743573d
Massimiliano Marcon <me@marcon.me>
2015-04-27 13:59:33 +0200

    Adds option of passing the database file.

---
+++ example/example-localfile.js
@@ -0,0 +1,23 @@
+var keyring = require('../keyring'),
+    path = require('path');
+
+var foursquareURL = '';
+foursquareURL += 'https://api.foursquare.com/v2/venues/search?';
+foursquareURL += 'radius={RADIUS}&ll={LOCATION}&limit={LIMIT}&v=20120927';
+foursquareURL += '&client_id={ID}&client_secret={SECRET}';
+
+var db = path.join(__dirname, 'db.json');
+
+var keyringApi = keyring.instance(null/*no encryption key*/, db).load();
+
+//Before running the example, from the command line do the following:
+//  keyring store -k foursquare.myapp.clientId -v SOME_CLIENT_ID
+//  keyring store -k foursquare.myapp.clientSecret -v SOME_CLIENT_SECRET
+
+foursquareURL = foursquareURL.replace('{RADIUS}', 1500)
+                             .replace('{LOCATION}', '52.5,13.3')
+                             .replace('{LIMIT}', 50)
+                             .replace('{ID}', keyringApi.retrieve('foursquare.myapp.clientId'))
+                             .replace('{SECRET}', keyringApi.retrieve('foursquare.myapp.clientSecret'));
+
+console.log(foursquareURL);
